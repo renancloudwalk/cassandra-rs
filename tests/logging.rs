f8fda852c51528a36f3e6c35a7f9300eb5c985ef
Lucas Kent <rubickent@gmail.com>
2022-10-07 16:35:18 +1100

    Add support for logging via the log crate.
    Signed-off-by: Lucas Kent <rubickent@gmail.com>

--- tests/logging.rs
+++ tests/logging.rs
@@ -2,6 +2,7 @@ mod help;
 
 use cassandra_cpp::*;
 
+#[cfg(feature = "slog")]
 use slog::*;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -16,6 +17,7 @@ impl Default for MyDrain {
     }
 }
 
+#[cfg(feature = "slog")]
 impl Drain for MyDrain {
     type Ok = ();
     type Err = ();
@@ -33,19 +35,20 @@ impl Drain for MyDrain {
     }
 }
 
-#[tokio::test]
-async fn test_logging() {
+#[cfg(feature = "slog")]
+#[test]
+fn test_slog_logger() {
     let drain = MyDrain::default();
     let logger = Logger::root(drain.clone().fuse(), o!());
 
     set_level(LogLevel::WARN);
-    set_logger(Some(logger));
+    set_slog_logger(logger);
 
     let mut cluster = Cluster::default();
     cluster
         .set_contact_points("absolute-gibberish.invalid")
         .unwrap();
-    cluster.connect().await.expect_err("Should fail to connect");
+    cluster.connect().expect_err("Should fail to connect");
 
     let log_output: String = drain.0.lock().unwrap().clone();
     assert!(
@@ -55,29 +58,63 @@ async fn test_logging() {
     );
 }
 
-#[tokio::test]
-async fn test_metrics() {
+#[cfg(feature = "log")]
+#[test]
+fn test_log_logger() {
+    use log::Level;
+
+    let mut logger = logtest::Logger::start();
+    set_level(LogLevel::WARN);
+    set_log_logger();
+
+    let mut cluster = Cluster::default();
+    cluster
+        .set_contact_points("absolute-gibberish.invalid")
+        .unwrap();
+    cluster.connect().expect_err("Should fail to connect");
+
+    let record = logger.pop().unwrap();
+    assert_eq!(
+        record.args(),
+        "Unable to resolve address for absolute-gibberish.invalid:9042\n",
+    );
+    assert_eq!(record.level(), Level::Error);
+    assert_eq!(
+        record.target(),
+        "{anonymous}::DefaultClusterMetadataResolver::on_resolve"
+    );
+    assert_eq!(record.key_values(), vec!());
+}
+
+#[test]
+fn test_metrics() {
     // This is just a check that metrics work and actually notice requests.
     // Need to send a cassandra query that will produce a positive number for the min_us metric
     // (minimum time to respond to a request in microseconds), i.e. a request that make cassandra
     // take more than 1 microsecond to respond to. Do a couple of setup queries, with IF NOT EXISTS
     // so that they don't fail if this test is repeated.
-    let query1 = "CREATE KEYSPACE IF NOT EXISTS cycling WITH REPLICATION = {
+    let query1 = stmt!(
+        "CREATE KEYSPACE IF NOT EXISTS cycling WITH REPLICATION = {
                       'class' : 'SimpleStrategy',
                       'replication_factor' : 1
-                      };";
-    let query2 = "CREATE TABLE IF NOT EXISTS cycling.cyclist_name (
+                      };"
+    );
+    let query2 = stmt!(
+        "CREATE TABLE IF NOT EXISTS cycling.cyclist_name (
                        id UUID PRIMARY KEY,
                        lastname text,
-                       firstname text );"; //create table
-    let query3 = "INSERT INTO cycling.cyclist_name (id, lastname, firstname)
+                       firstname text );"
+    ); //create table
+    let query3 = stmt!(
+        "INSERT INTO cycling.cyclist_name (id, lastname, firstname)
                        VALUES (6ab09bec-e68e-48d9-a5f8-97e6fb4c9b47, 'KRUIKSWIJK','Steven')
-                       USING TTL 86400 AND TIMESTAMP 123456789;";
+                       USING TTL 86400 AND TIMESTAMP 123456789;"
+    );
 
-    let session = help::create_test_session().await;
-    session.execute(&query1).await.unwrap();
-    session.execute(&query2).await.unwrap();
-    session.execute(&query3).await.unwrap();
+    let session = help::create_test_session();
+    session.execute(&query1).wait().unwrap();
+    session.execute(&query2).wait().unwrap();
+    session.execute(&query3).wait().unwrap();
 
     let metrics = session.get_metrics();
     assert_eq!(metrics.total_connections, 1);
